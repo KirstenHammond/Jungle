f152af9a3c11d81e48dd0980a61bb985573fea85
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2023-01-31 14:24:06 -0800

    cypress home page tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,20 @@
+describe('Jungle web app', () => {
+
+  // beforeEach(() => {
+  //   cy.visit('http://0.0.0.0:3000/products')
+  // })
+
+  it("can visit the home page", () => {
+    cy.visit('http://0.0.0.0:3000')
+  })
+
+  it("There are products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  })
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  })
+
+
+})
