a6fddbf61d4b70437c4d62bfa6c011d2ad5e7c5c
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2023-01-30 17:34:43 -0800

    add rspec test widget specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
   def create
-     
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
@@ -11,6 +10,7 @@ class UsersController < ApplicationController
     redirect_to '/users/new'
    end
   end
+
   private
 
   def user_params
